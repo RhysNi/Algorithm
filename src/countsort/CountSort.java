880ca00ddea20c297173cb8c5b003fcd0f4f2b18
RhysNi <java_nsd@163.com>
2024-04-29 01:44:12 +0800

    计数排序

---
+++ src/countsort/CountSort.java
@@ -0,0 +1,116 @@
+package countsort;
+
+import java.util.Arrays;
+
+/**
+ * @author Rhys.Ni
+ * @version 1.0
+ * @date 2024/4/28 11:49 PM
+ */
+public class CountSort {
+    public static void main(String[] args) {
+        int testTime = 500000;
+        int maxSize = 100;
+        int maxValue = 150;
+        boolean succeed = true;
+        for (int i = 0; i < testTime; i++) {
+            int[] arr1 = generateRandomArray(maxSize, maxValue);
+            int[] arr2 = copyArray(arr1);
+            countSort(arr1);
+            comparator(arr2);
+            if (!isEqual(arr1, arr2)) {
+                succeed = false;
+                printArray(arr1);
+                printArray(arr2);
+                break;
+            }
+        }
+        System.out.println(succeed ? "Nice!" : "Fucking fucked!");
+
+        int[] arr = generateRandomArray(maxSize, maxValue);
+        printArray(arr);
+        countSort(arr);
+        printArray(arr);
+    }
+
+    public static void countSort(int[] arr) {
+        if (arr == null || arr.length < 2) {
+            return;
+        }
+        // 让最大值从最小值开始
+        int max = Integer.MIN_VALUE;
+        //找出年龄最大的
+        for (int value : arr) {
+            max = Math.max(max, value);
+        }
+        // 初始化桶容量，保证能存max+1个数量的元素（0-100岁员工存在索引中因此对应101个元素）
+        int[] bucket = new int[max + 1];
+        // 统计每个年龄段出现的次数（每个年龄段的人数），出现一次在桶中对应的数量就+1
+        for (int k : arr) {
+            bucket[k]++;
+        }
+        int i = 0;
+        // 从桶中往外倒
+        for (int j = 0; j < bucket.length; j++) {
+            while (bucket[j]-- > 0) {
+                arr[i++] = j;
+            }
+        }
+    }
+
+    // for test
+    public static void comparator(int[] arr) {
+        Arrays.sort(arr);
+    }
+
+    // for test
+    public static int[] generateRandomArray(int maxSize, int maxValue) {
+        int[] arr = new int[(int) ((maxSize + 1) * Math.random())];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = (int) ((maxValue + 1) * Math.random());
+        }
+        return arr;
+    }
+
+    // for test
+    public static int[] copyArray(int[] arr) {
+        if (arr == null) {
+            return null;
+        }
+        int[] res = new int[arr.length];
+        for (int i = 0; i < arr.length; i++) {
+            res[i] = arr[i];
+        }
+        return res;
+    }
+
+    // for test
+    public static boolean isEqual(int[] arr1, int[] arr2) {
+        if ((arr1 == null && arr2 != null) || (arr1 != null && arr2 == null)) {
+            return false;
+        }
+        if (arr1 == null && arr2 == null) {
+            return true;
+        }
+        if (arr1.length != arr2.length) {
+            return false;
+        }
+        for (int i = 0; i < arr1.length; i++) {
+            if (arr1[i] != arr2[i]) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    // for test
+    public static void printArray(int[] arr) {
+        if (arr == null) {
+            return;
+        }
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+}
